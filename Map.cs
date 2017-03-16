9b3b6c30eb263c00408060a01c5f19468e502ed8
Coder1048576 <anshul.cgm@gmail.com>
2017-03-15 22:24:27 -0700

    Add files via upload
    Map now includes a method to check if a hazard is two or less rooms away from the player

--- Map.cs
+++ Map.cs
@@ -11,7 +11,8 @@ namespace WumpusTest
         private int wumpusLocation;
         private int playerLocation;
         private int[] batsLocations;
-        private int[] bottomlesspitlocations;
+        private int[] bottomlesspitLocations;
+        private Cave cave;
         
         public Map()
         {
@@ -34,16 +35,25 @@ namespace WumpusTest
             {
                 batsLocations[1] = gen.Next(30) + 1;
             }
-            bottomlesspitlocations[0] = gen.Next(30) + 1;
-            while (bottomlesspitlocations[0] == playerLocation || bottomlesspitlocations[0] == wumpusLocation || bottomlesspitlocations[0] == batsLocations[0] || bottomlesspitlocations[0] == batsLocations[1])
+            bottomlesspitLocations[0] = gen.Next(30) + 1;
+            while (bottomlesspitLocations[0] == playerLocation || bottomlesspitLocations[0] == wumpusLocation || bottomlesspitLocations[0] == batsLocations[0] || bottomlesspitLocations[0] == batsLocations[1])
             {
-                bottomlesspitlocations[0] = gen.Next(30) + 1;
+                bottomlesspitLocations[0] = gen.Next(30) + 1;
             }
-            bottomlesspitlocations[1] = gen.Next(30) + 1;
-            while (bottomlesspitlocations[1] == playerLocation || bottomlesspitlocations[1] == wumpusLocation || bottomlesspitlocations[1] == batsLocations[1] || bottomlesspitlocations[1] == batsLocations[1] || bottomlesspitlocations[1] == bottomlesspitlocations[0])
+            bottomlesspitLocations[1] = gen.Next(30) + 1;
+            while (bottomlesspitLocations[1] == playerLocation || bottomlesspitLocations[1] == wumpusLocation || bottomlesspitLocations[1] == batsLocations[1] || bottomlesspitLocations[1] == batsLocations[1] || bottomlesspitLocations[1] == bottomlesspitLocations[0])
             {
-                bottomlesspitlocations[1] = gen.Next(30) + 1;
+                bottomlesspitLocations[1] = gen.Next(30) + 1;
             }
+            cave = new Cave();
+        }
+        public Map(int playerLocation, int wumpusLocation, int[] batsLocations, int[] bottomlesspitLocations)
+        {
+            this.playerLocation = playerLocation;
+            this.wumpusLocation = wumpusLocation;
+            this.batsLocations = batsLocations;
+            this.bottomlesspitLocations = bottomlesspitLocations;
+            cave = new Cave();
         }
         public void movesForward(int room_number)
         {
@@ -61,7 +71,7 @@ namespace WumpusTest
             {
                 return 2;
             }
-            else if(playerLocation== bottomlesspitlocations[0] || playerLocation == bottomlesspitlocations[1])
+            else if(playerLocation== bottomlesspitLocations[0] || playerLocation == bottomlesspitLocations[1])
             {
                 return 1;
             }
@@ -85,7 +95,7 @@ namespace WumpusTest
             //array will look like [(isWumpusCloseTrueOrFalse), (isABatCloseTrueOrFalse), (isTheOtherBatCloseTrueOrFalse), 
             //(is theBottomLessPitCloseTrueOrFalse, IsTheOtherBottomLessPitCloseTrueOrFalse)
             bool[] hazards = new bool[5];
-            if(distanceBetweenRooms(playerLocation, wumpusLocation) <= 2)
+            if(distanceBetweenRooms(playerLocation, wumpusLocation) == true)
             {
                 hazards[0] = true;
             }
@@ -93,7 +103,7 @@ namespace WumpusTest
             {
                 hazards[0] = false;
             }
-            if(distanceBetweenRooms(playerLocation, batsLocations[0]) <= 2)
+            if(distanceBetweenRooms(playerLocation, batsLocations[0]) == true)
             {
                 hazards[1] = true;
             }
@@ -101,7 +111,7 @@ namespace WumpusTest
             {
                 hazards[1] = false;
             }
-            if (distanceBetweenRooms(playerLocation, batsLocations[1]) <= 2)
+            if (distanceBetweenRooms(playerLocation, batsLocations[1]) == true)
             {
                 hazards[2] = true;
             }
@@ -109,7 +119,7 @@ namespace WumpusTest
             {
                 hazards[2] = false;
             }
-            if (distanceBetweenRooms(playerLocation, bottomlesspitlocations[0]) <= 2)
+            if (distanceBetweenRooms(playerLocation, bottomlesspitLocations[0]) == true)
             {
                 hazards[3] = true;
             }
@@ -117,7 +127,7 @@ namespace WumpusTest
             {
                 hazards[3] = false;
             }
-            if (distanceBetweenRooms(playerLocation, bottomlesspitlocations[1]) <= 2)
+            if (distanceBetweenRooms(playerLocation, bottomlesspitLocations[1]) == true)
             {
                 hazards[4] = true;
             }
@@ -141,13 +151,84 @@ namespace WumpusTest
         }
         public int[] getBottomLessPitLocations()
         {
-            return bottomlesspitlocations;
+            return bottomlesspitLocations;
         }
-        public static int distanceBetweenRooms(int room1, int room2)
+        public bool distanceBetweenRooms(int room1, int room2)
         {
-            //find distance between two rooms and return as an integer
-            return 0;
-        }
+            //If distance between rooms is equal to 1, or 2 returns true
+            int[,] arrayOfRooms = cave.getRoomConnections();
+            //Gets room connection array from cave
+            int[] roomArray = new int[6];
+            //Creates connection array for first room
+            bool hazard = false;
+            //Sets return variable to false so if hazard is not two rooms away, method
+            //will return false
+            for (int i = 0; i < roomArray.Length; i++)
+            {
+                roomArray[i] = Math.Abs(arrayOfRooms[room1 - 1, i]);
+                //gets connections of room1 from 2D array returned by Cave Object
+            }
+            for(int i = 0; i < roomArray.Length; i++)
+            {
+                if(room2 == roomArray[i])
+                {
+                    hazard = true;
+                }
+            }
+            //Above for loop checks if room2 can be found in rooms that are one room away from room1
+            int[] roomArray1 = new int[6];
+            for(int i = 0; i < roomArray.Length; i++)
+            {
+                roomArray1[i] = arrayOfRooms[roomArray[0], i];
+            }
+            //Gets connections for first room in roomArray
+            int[] roomArray2 = new int[6];
+            for (int i = 0; i < roomArray.Length; i++)
+            {
+                roomArray2[i] = arrayOfRooms[roomArray[1], i];
 
+            }
+            //Gets connections for second room in roomArray
+            int[] roomArray3 = new int[6];
+            for (int i = 0; i < roomArray.Length; i++)
+            {
+                roomArray3[i] = arrayOfRooms[roomArray[2], i];
+            }
+            //Gets connections for third room in roomArray
+            int[] roomArray4 = new int[6];
+            for (int i = 0; i < roomArray.Length; i++)
+            {
+                roomArray1[i] = arrayOfRooms[roomArray[3], i];
+            }
+            //Gets connections for fourth room in roomArray
+            int[] roomArray5 = new int[6];
+            for (int i = 0; i < roomArray.Length; i++)
+            {
+                roomArray5[i] = arrayOfRooms[roomArray[4], i];
+            }
+            //gets connections for fifth room in roomArray
+            int[] roomArray6 = new int[6];
+            for (int i = 0; i < roomArray.Length; i++)
+            {
+                roomArray6[i] = arrayOfRooms[roomArray[5], i];
+            }
+            //gets connections for sixth room in roomArray
+            int[][] roomsTwoAway = new int[][] { roomArray1, roomArray2, roomArray3, roomArray4, roomArray5, roomArray6 };
+            //creates an array of arrays for rooms that are a distance 
+            //of two rooms away from room1
+            for(int i = 0; i < roomsTwoAway.Length; i++)
+            {
+                for(int j = 0; j < roomsTwoAway[i].Length; j++)
+                {
+                    if(roomsTwoAway[i][j] == room2)
+                    {
+                        hazard = true;
+                    }
+                }
+            }
+            //Checks to see if room2 is in any of the arrays.
+            //If so sets hazard to true
+            return hazard;
+        }
     }
 }
