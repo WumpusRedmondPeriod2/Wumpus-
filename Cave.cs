7f1b31c960914ee1dd831a8b4c96fd2c8fa02a0b
Coder1048576 <anshul.cgm@gmail.com>
2017-03-30 08:57:48 -0700

    Add files via upload

--- Cave.cs
+++ Cave.cs
@@ -1,22 +1,40 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace WumpusTest
 {
     class Cave
     {
-        public Cave()
-        { }
-        private bool hasEntered;
-        public bool[] pathWays(int CurrentRoom)
-        {
-            bool[] x = new bool[6];
 
-            return x;
+        private static int[,] x1 = new int[30, 6];
+
+        public Cave(int a)
+        {
+            if (a == 1)
+            {
+                String FileName = "Cave" + a + ".txt";
+                System.IO.StreamReader file = new System.IO.StreamReader(@"\Projects\WumpusTestV2\WumpusTestV2\" + FileName);
+                while (file.ReadLine() != null)
+                {
+                    int[,] x = new int[30, 6];
+                    for (int i = 0; i < 30; i++)
+                    {
+                        String[] st = file.ReadLine().Split(' ');
+                        for (int j = 0; j < 6; j++)
+                        {
+                            x[i, j] = int.Parse(st[i]);
+                        }
+                    }
+                    x1 = x;
+                }
+            }
+        }
+        public int[,] getRoomConnections()
+        {
+            return x1;
         }
     }
 }
