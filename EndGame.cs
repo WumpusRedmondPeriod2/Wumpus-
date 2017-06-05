7183d4999dd61dd130b92ac89cf51a53288e89d1
Coder1048576 <anshul.cgm@gmail.com>
2017-06-04 19:39:45 -0700

    Add files via upload

---
+++ EndGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WumpusTest
+{
+    public partial class EndGame : Form
+    {
+        public EndGame(int score)
+        {
+            InitializeComponent();
+            label2.Text = score.ToString();
+        }
+
+        private void EndGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form1 = new StartGame();
+            form1.Closed += (s, args) => this.Close();
+            form1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();  
+        }
+    }
+}
