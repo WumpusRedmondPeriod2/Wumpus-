cfdb1daef191be227206f9f34b18adafba504c7c
s-mhe <s-mhe@lwsd.org>
2017-06-06 23:11:01 -0700

    Add files via upload

--- Cave.cs
+++ Cave.cs
@@ -8,22 +8,23 @@ namespace WumpusTest
 {
     class Cave
     {
-
+        //2D array of the pathways between rooms
         private static int[,] pathways = new int[30, 6];
 
+        //constructor - takes the cave number as parameter
         public Cave(int a)
         {
+            //gets cave based on cave number
             string FileName = "Cave" + a + ".txt";
-            string[] stPathways = System.IO.File.ReadAllLines(@"" +FileName);
-            //reads from any file
+            string[] stPathways = System.IO.File.ReadAllLines(@"" + FileName);
+            //checks if this is null and throws exception if found
             if (stPathways == null)
             {
                 throw new IOException("Unable to read from file");
-                //checks if this is null and throws exception if found
             }
+            //sets the pathways for the current cave
             else
             {
-
                 int[,] temporaryPathways = new int[30, 6];
                 for (int i = 0; i < 30; i++)
                 {
@@ -36,7 +37,8 @@ namespace WumpusTest
                 pathways = temporaryPathways;
             }
         }
-
+        
+        //returns a 2D array of the pathways between rooms
         public int[,] getRoomConnections()
         {
             return pathways;
