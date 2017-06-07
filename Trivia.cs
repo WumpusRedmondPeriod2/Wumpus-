69372946e97050a9105225b5681272363e1a812e
s-mhe <s-mhe@lwsd.org>
2017-06-06 22:55:01 -0700

    Add files via upload

--- Trivia.cs
+++ Trivia.cs
@@ -5,14 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WumpusTest
-
 {
     class Trivia
     {
-        private string question;
+        private String question;
         private static Boolean[] elimQuestions = new Boolean[20];
-        private string[] questionArr;
-        private string[] stringArray;
+        private String[] questionArr;
+        private String[] stringArray;
 
         public Trivia()
         {
@@ -20,65 +19,52 @@ namespace WumpusTest
             questionArr = System.IO.File.ReadAllLines(@"TriviaQuestions.txt");
             //Sets first question as current question
             question = questionArr[0];
+            //splits each line into questions and answers
             stringArray = question.Split(';');
-            for(int i = 0; i < elimQuestions.Length; i++)
+            //resets questions so all questions can be shown
+            for (int i = 0; i < elimQuestions.Length; i++)
             {
                 elimQuestions[i] = false;
             }
+            //eliminates the first question
             elimQuestions[0] = true;
         }
-        public string getQuestion()
+
+        //returns the current question as a String
+        public String getQuestion()
         {
-            //Sends question as a String to Game Control
-            return stringArray[0]; 
+            return stringArray[0];
         }
 
-        public string getAnswer()
+        //returns answer choices as a string array
+        public String[] getPossibleAnswers()
         {
-            //returns the answer as a string to Game Control
-            return stringArray[5];
-        }
-
-        public string[] getPossibleAnswers() 
-        {
-            //returns answer choices as a string array
-            string[] returnArray = new string[5];
+            String[] returnArray = new string[5];
 
             for (int i = 0; i < 5; i++)
             {
-                returnArray[i] = stringArray[i+1];
+                returnArray[i] = stringArray[i + 1];
             }
-            return returnArray; 
+            return returnArray;
         }
 
+        //loads the next question
         public void newQuestion()
         {
-            Random r = new Random();
             //randomizes question 
+            Random r = new Random();
             int q = r.Next(20);
 
+            //ensures answered questions are not repeated
             while (elimQuestions[q] != false)
             {
                 q = r.Next(20);
             }
-            //ensures answered questions are not repeated
             elimQuestions[q] = true;
             //sets new question and answers as current question and answer
             question = questionArr[q];
             stringArray = question.Split(';');
         }
-
-        public Boolean isAnswerCorrect(String answer)
-        {
-            //Determines if answer is correct for game control
-            if (stringArray[5] == answer)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
+
